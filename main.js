654942dd3331a926b62b080c6c08d40754c729db
NKGrimm404 <87152727+NKGrimm404@users.noreply.github.com>
2022-03-31 17:45:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+}
+
+function take_snapshot(){
+    save('sumolol.png');
+
+}
